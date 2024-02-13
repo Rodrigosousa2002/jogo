846efce01fdb3c6c062aef381d0d3307411f8781
Rodrigosousa2002 <rodrigo.sousa4445@gmail.com>
2024-02-13 13:05:53 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,12 @@
+y = int(input("digite o valor de y: "))
+k = int(input("digite o valor de k: "))
+z = int(input("digite o valor de z: "))
+x=(y+k+3*z)/4
+print("x", x)
+
+#próximo código
+valororig = int(input("digite um valor: "))
+desc =  5
+desc= desc/100*valororig
+valorfinal= desc-valororig
+print("valorfinal", valorfinal)
